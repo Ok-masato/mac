8e76179b50cd9c1bd18b0f97deb5935428863d6f
okmas <okmasato@outlook.jp>
2021-06-05 04:08:12 +0900

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,62 @@
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   // Firebase初期化
   await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Test',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primaryColor: Colors.blueGrey[800],
+        primarySwatch: Colors.blue,
       ),
+      home: MyAuthPage(),
     );
   }
 }
 
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // primarySwatch:  Colors.grey,
-//         primaryColor: Colors.blueGrey[800],
-//       ),
-//       // home: MyAuthPage(),
-//       home: RootPage(),
-//       routes: <String,WidgetBuilder>{
-//         '/home':(BuildContext context) => HomePage(),
-//         '/register':(BuildContext context) => RegisterPage(),
-//         '/login':(BuildContext context) => LoginPage(),
-//       }
-//     );
-//   }
-// }
+class MyAuthPage extends StatefulWidget {
+  @override
+  _MyAuthPageState createState() => _MyAuthPageState();
+}
 
-// class MyAuthPage extends StatefulWidget {
-//   @override
-//   _MyAuthPageState createState() => _MyAuthPageState();
-// }
-//
-// class _MyAuthPageState extends State<MyAuthPage> {
-//   // 入力されたメールアドレス（ログイン）
-//   String loginUserEmail = "";
-//   // 入力されたパスワード（ログイン）
-//   String loginUserPassword = "";
-//   // 登録・ログインに関する情報を表示
-//   String infoText = "";
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("トップページ"),
-//         actions: <Widget>[
-//           ButtonTheme(
-//             minWidth: 100,
-//             height: 20,
-//             child: RaisedButton(
-//               child: Text("ログイン"),
-//               color: Colors.redAccent,
-//               onPressed: () => {
-//                 // ボタンを押された時に新規登録画面へ遷移する
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(builder: (context) => LoginPage()),
-//                 ),
-//               },
-//             ),
-//           )
-//         ],
-//       ),
-//       body: Center(
-//           child: Container(
-//               padding: EdgeInsets.all(32),
-//               child: Column(
-//                   children: <Widget>[
-//                     Center(
-//                       child: Column(
-//                         children: <Widget>[
-//                           Text("home page"),
-//                         ],
-//                       ),
-//                     ),
-//                   ],
-//               ),
-//           ),
-//       ),
-//     );
-//   }
-// }
+class _MyAuthPageState extends State<MyAuthPage> {
+  // 入力されたメールアドレス
+  String newUserEmail = "";
+  // 入力されたパスワード
+  String newUserPassword = "";
+  // 登録・ログインに関する情報を表示
+  String infoText = "";
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: "パスワード（6文字以上）"),
+                // パスワードが見えないようにする
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    newUserPassword = value;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
